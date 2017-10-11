034ba39d0be1c32bf782dfe708094755c7342448
Vendin Alex <alexvendin@gmail.com>
2017-10-11 22:34:07 +0300

    sources for example from thinking in java with spring and hibernate

---
+++ src/main/java/com/github/alexv1993/literals/main/ShowManager.java
@@ -0,0 +1,20 @@
+package com.github.alexv1993.literals.main;
+
+/**
+ * Created by vendin on 11.10.2017.
+ */
+public interface ShowManager {
+    public void show(int i);
+
+    public void show(char i);
+
+    public void show(byte i);
+
+    public void show(short i);
+
+    public void show(long i);
+
+    public void show(float i);
+
+    public void show(double i);
+}
