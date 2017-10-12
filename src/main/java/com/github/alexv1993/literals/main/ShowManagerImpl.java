000bc2bf0b3c3a05056a6f12379e1c334c82d34c
Vendin Alex <alexvendin@gmail.com>
2017-10-12 21:38:41 +0300

    sources for example from thinking in java with spring and hibernate

--- src/main/java/com/github/alexv1993/literals/main/ShowManagerImpl.java
+++ src/main/java/com/github/alexv1993/literals/main/ShowManagerImpl.java
@@ -1,42 +1,12 @@
 package com.github.alexv1993.literals.main;
 
+
 /**
  * Created by vendin on 11.10.2017.
  */
-public class ShowManagerImpl implements ShowManager {
+public class ShowManagerImpl<T> implements ShowManager<T> {
     @Override
-    public void show(int i) {
-        System.out.println("i " + Long.toBinaryString(i));
-    }
-
-    @Override
-    public void show(char i) {
-        System.out.println("i " + Long.toBinaryString(i));
-    }
-
-    @Override
-    public void show(byte i) {
-        System.out.println("i " + Long.toBinaryString(i));
-    }
-
-    @Override
-    public void show(short i) {
-        System.out.println("i " + Long.toBinaryString(i));
-    }
-
-    @Override
-    public void show(long i) {
+    public void show(T i) {
         System.out.println("i " + i);
     }
-
-    @Override
-    public void show(float i) {
-        System.out.println("i " + i);
-    }
-
-    @Override
-    public void show(double i) {
-        System.out.println("i " + i);
-    }
-
 }
